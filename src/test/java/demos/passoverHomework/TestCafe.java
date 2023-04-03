5cc7c4d810c0c60604251bc2d3d37f1fdda2cf29
Dedru142 <dandan999111@gmail.com>
2023-04-03 23:40:05 +0300

    added TestCafe homework

---
+++ src/test/java/demos/passoverHomework/TestCafe.java
@@ -0,0 +1,43 @@
+package demos.passoverHomework;
+
+import com.microsoft.playwright.*;
+
+public class TestCafe {
+
+    public static void main(String[] args) {
+
+        Playwright playwright = Playwright.create();
+
+        Browser browser = playwright.firefox().launch(
+                new BrowserType.LaunchOptions()
+                        .setHeadless(false)
+                        .setSlowMo(100)
+        );
+        Page newPage = browser.newPage();
+
+        newPage.navigate("https://devexpress.github.io/testcafe/example/");
+
+        newPage.locator("[data-testid=\"name-input\"]").type("Dan");
+
+        String[] checkboxes = {"remote-testing-checkbox", "reusing-js-code-checkbox", "parallel-testing-checkbox", "ci-checkbox", "analysis-checkbox", "tried-testcafe-checkbox"};
+        for (String checkbox : checkboxes) {
+            newPage.locator("[data-testid=\"" + checkbox + "\"]").click();
+        }
+
+        newPage.locator("[data-testid=\"comments-area\"]").type("something!");
+
+        newPage.locator("[data-testid=\"submit-button\"]").click();
+
+        String thanks = newPage.locator("[data-testid=\"thank-you-header\"]").textContent();
+        String url = newPage.url();
+        String title = newPage.title();
+
+        System.out.println(thanks);
+        System.out.println("url = " + url);
+        System.out.println("title = " + title);
+
+        newPage.close();
+        browser.close();
+        playwright.close();
+    }
+}
